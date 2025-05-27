42f3e51f8abb19226211160260bd6a1add731be3
kareraolivier <kareraolivier@gmail.com>
2025-05-27 12:05:55 -0700

    fix: naming and sizing

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Homepage = () => {
   return (
-    <div className="relative h-[50vh] md:h-[66vh] w-full border-b-2 border-primary">
+    <div className="relative h-[40vh] w-full border-b-2 border-primary">
       <Image
         src="/image/hero.png"
         alt="Hero"
@@ -11,7 +11,7 @@ const Homepage = () => {
         priority
       />
       <div className="absolute inset-0 flex items-center justify-center bg-black/40">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl text-white text-center px-4 w-full md:w-4/5 lg:w-2/5">
+        <h1 className="text-2xl md:text-3xl lg:text-5xl font-normal text-white text-center px-4 w-full md:w-4/5 lg:w-2/5">
           We build and co-create digital ventures
         </h1>
       </div>
--- src/components/TextBlock/index.tsx
+++ src/components/TextBlock/index.tsx
@@ -6,9 +6,9 @@ interface TextBlockProps {
 const TextBlock = ({ title, description }: TextBlockProps) => {
   return (
     <div className="flex flex-col gap-14 mt-16">
-      <h1 className="text-3xl font-normal">{title}</h1>
-      <p className="font-light text-2xl">{description}</p>
-      <hr className="w-1/12 border-1 border-primary border-dashed" />
+      <h1 className="text-3xl font-medium">{title}</h1>
+      <p className="text-2xl text-secondary font-extralight">{description}</p>
+      <hr className="w-1/3 md:w-1/6 lg:w-1/12 border-1 border-primary border-dashed" />
     </div>
   );
 };
