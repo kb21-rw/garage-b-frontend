d53e532f5b3a7e0d24ebde1ae70cc38093b3c8c9
kareraolivier <kareraolivier@gmail.com>
2025-05-26 20:20:44 -0700

    feat: add footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -4,10 +4,10 @@ const Footer = () => {
   return (
     <div className="container flex flex-col justify-center items-center text-secondary">
       <hr className="border-1 border-primary w-full" />
-      <div className="flex justify-center items-center gap-18 py-18">
+      <div className="flex justify-start md:justify-center items-center gap-x-18 gap-y-11 py-18 flex-wrap">
         <h1 className="text-2xl font-bold">Find us on :</h1>
         <div className="flex flex-col gap-11">
-          <div className="flex gap-4">
+          <div className="flex gap-4 flex-wrap">
             {[1, 2, 3, 4, 5].map((el) => {
               return (
                 <Image
@@ -16,15 +16,16 @@ const Footer = () => {
                   alt="image"
                   width={40}
                   height={40}
+                  layout="contain"
                   className="cursor-pointer"
                 />
               );
             })}
           </div>
           <p className="text-2xl">
-            Or reach out to
+            Or reach out to: {""}
             <a
-              href="contact@the-gym.rw"
+              href="mail:contact@the-gym.rw"
               target="_blank"
               className="hover:text-primary"
             >
