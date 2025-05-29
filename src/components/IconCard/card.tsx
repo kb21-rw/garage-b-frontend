cec7cc76b70a222f1787a3857c9da52c094dec35
kareraolivier <kareraolivier@gmail.com>
2025-05-29 12:06:33 -0700

    fix: styles across the whole platform

--- src/components/IconCard/card.tsx
+++ src/components/IconCard/card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 
 const Card = ({ title, icon, description }: CardProps) => {
   return (
-    <div className="flex flex-col h-full gap-5 xl:gap-10 bg-black-300 border-l border-primary px-12 pb-12 pt-3 min-h-100">
+    <div className="flex flex-col h-full gap-5 xl:gap-10 bg-black-300 border-l border-primary px-6 md:px-12 pb-6 md:pb-12 pt-3 min-h-100">
       <Image src={icon} alt="icon" width={80} height={80} />
       <h1 className="text-2xl lg:text-3xl font-medium">{title}</h1>
       <p className="text-lg lg:text-2.1xl text-secondary font-extralight text-left leading-7.5 tracking-1.5 flex-grow">
