883e41480526d31c125df9dd1fbed7376abfb848
kareraolivier <kareraolivier@gmail.com>
2025-06-10 17:47:29 +0200

    fix: deployment

--- src/app/404.tsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <h1 className="text-4xl font-bold">404</h1>
-      <p>Page Not Found</p>
-    </div>
-  );
-};
-export default NotFound;
