328ebfc5aaedef9f9bf3efe2facd9a3605fa8a09
kareraolivier <kareraolivier@gmail.com>
2025-06-23 10:56:11 +0200

    feat: improuving the mobile slide

--- src/components/ImageSliderBlock/index.tsx
+++ src/components/ImageSliderBlock/index.tsx
@@ -5,33 +5,61 @@ import Card from "./card";
 import Marquee from "react-fast-marquee";
 import RichText from "../ui/RichText";
 
-const IconSlider = ({ header, images }: ImageSliderSectionBlock) => {
+interface ResponsiveSliderProps {
+  images: ImageSliderSectionBlock["images"];
+  threshold: number;
+}
+
+const ResponsiveSlider: React.FC<ResponsiveSliderProps> = ({
+  images,
+  threshold,
+}) => {
+  const shouldMarquee = images.length > threshold;
+
+  return (
+    <>
+      {shouldMarquee ? (
+        <Marquee pauseOnHover loop={0}>
+          {images.map((icon, idx) => (
+            <Card key={idx} {...icon} />
+          ))}
+        </Marquee>
+      ) : (
+        <div className="flex justify-start flex-wrap gap-8">
+          {images.map((icon, idx) => (
+            <Card key={idx} {...icon} />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+const ImageSlider = ({ header, images }: ImageSliderSectionBlock) => {
   return (
     <div className="flex flex-col gap-14 mt-16">
-      <h1 className="text-2xl lg:text-3xl font-medium">Our Partners</h1>
+      <h1 className="text-2xl lg:text-3xl font-medium">{header.title}</h1>
       {header.description && (
         <div className="text-lg lg:text-2.1xl text-secondary font-extralight leading-7.5 tracking-1.5">
           <RichText content={header.description} />
         </div>
       )}
       <div className="overflow-hidden w-full py-4">
-        {images.length > 5 ? (
-          <Marquee pauseOnHover loop={0}>
-            {images.map((icon, idx) => (
-              <Card key={idx} {...icon} />
-            ))}
-          </Marquee>
-        ) : (
-          <div className="flex justify-start flex-wrap gap-8">
-            {images.map((icon, idx) => (
-              <Card key={idx} {...icon} />
-            ))}
-          </div>
-        )}
+        <div className="block md:hidden">
+          <ResponsiveSlider images={images} threshold={1} />
+        </div>
+
+        <div className="hidden md:block lg:hidden">
+          <ResponsiveSlider images={images} threshold={3} />
+        </div>
+
+        <div className="hidden lg:block">
+          <ResponsiveSlider images={images} threshold={5} />
+        </div>
       </div>
       <hr className="w-1/3 md:w-1/6 lg:w-1/12 border-1 border-primary border-dashed" />
     </div>
   );
 };
 
-export default IconSlider;
+export default ImageSlider;
